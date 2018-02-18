6f174a50a6d1f8349c0766381119bae1f10a1be6
Tiago Portela <tiagoporteladesouza@gmail.com>
2018-02-18 15:17:28 +0100

    Modifying folders hierarchy

--- src/main/java/com/ubs/tiagoportela/messenger_ubs/main/MessengerUbsApplication.java
+++ src/main/java/com/ubs/tiagoportela/messenger_ubs/main/MessengerUbsApplication.java
@@ -1,12 +1,17 @@
 package com.ubs.tiagoportela.messenger_ubs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessengerUbsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(MessengerUbsApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Starting MessengerUbs service");
 		SpringApplication.run(MessengerUbsApplication.class, args);
 	}
 }
