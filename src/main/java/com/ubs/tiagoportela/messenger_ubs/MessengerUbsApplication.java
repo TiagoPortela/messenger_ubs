3a406a5d561878b622e430bd409bf47802343e8b
tiagoportela <tiagoporteladesouza@gmail.com>
2018-02-18 13:08:35 +0100

    Creating project structure with Spring Initializr

---
+++ src/main/java/com/ubs/tiagoportela/messenger_ubs/MessengerUbsApplication.java
@@ -0,0 +1,12 @@
+package com.ubs.tiagoportela.messenger_ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerUbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerUbsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ubs/tiagoportela/messenger_ubs/MessengerUbsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ubs.tiagoportela.messenger_ubs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessengerUbsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
